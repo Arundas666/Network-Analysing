14271e8ab840a94530c3c00835444ea8720e5614
Arundas666 <arundas285@gmail.com>
2024-03-03 23:55:54 +0530

    redis completed

--- backend/pkg/handlers/handler.go
+++ backend/pkg/handlers/handler.go
@@ -16,7 +16,6 @@ func FindAverageSpeedFromData(c *gin.Context) {
 
 	var rd = config.RedisConn
 	val, err := rd.Get(c, "country").Result()
-	fmt.Println("VAL", val)
 	if err == nil {
 		// Unmarshal the JSON string into an instance of CountryData
 		var countryData []response.ResponseData
@@ -65,17 +64,45 @@ func NetworkSpeedInStates(c *gin.Context) {
 	type Country struct {
 		Country string `json:"country"`
 	}
-	var a Country
+	var countryIp Country
 
-	if err := c.ShouldBindJSON(&a); err != nil {
+	if err := c.ShouldBindJSON(&countryIp); err != nil {
 		response.ErrorResponse(c, 400, "error in binding json", err, nil)
 		return
 	}
-	Id, _ := strconv.Atoi(a.Country)
-	fmt.Println(a, "ID", Id)
+	Id, _ := strconv.Atoi(countryIp.Country)
+
+	var rd = config.RedisConn
+	val, err := rd.Get(c, "state").Result()
+	if err == nil {
+		// Unmarshal the JSON string into an instance of CountryData
+		var stateData []response.ResponseData
+		err = json.Unmarshal([]byte(val), &stateData)
+		if err != nil {
+			response.ErrorResponse(c, 500, "failed to unmarshal data", err, nil)
+			return
+		}
+		// Now you can work with countryData as a Go struct
+		response.SuccessResponse(c, 200, "successfully fetched using Redis", stateData)
+		return
+	}
+
 	data, err := usecase.NetworkSpeedInsideCountry(Id)
 	if err != nil {
 		response.ErrorResponse(c, 400, "oops someting went wrong", err, nil)
+		return
+	}
+
+	dataString, err := json.Marshal(data)
+	if err != nil {
+		response.ErrorResponse(c, 500, "failed to serialize data", err, nil)
+		return
+	}
+
+	// Store the serialized data in Redis
+	err = rd.Set(c, "state", dataString, 0).Err()
+	if err != nil {
+		response.ErrorResponse(c, 500, "failed to store data in Redis", err, nil)
 		return
 	}
 	response.SuccessResponse(c, 200, "succesfully created data set", data)
